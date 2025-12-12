6f1b491f2e709fa73eddd1424d32990cec208a8a
Andres Sanchez <jorgeandresanchezperez@gmail.com>
2025-12-12 13:40:31 -0600

    Edit env

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ Bootstrap5(app)
 class Base(DeclarativeBase):
     pass
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DB_URL", 'sqlite:///posts.db')   
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
